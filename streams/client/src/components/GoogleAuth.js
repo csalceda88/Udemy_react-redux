628666e281aa7929f33bbf0d855ec4276f8c3881
csalceda88 <csalceda88@gmail.com>
2020-06-02 01:16:01 -0700

    oauth

--- streams/client/src/components/GoogleAuth.js
+++ streams/client/src/components/GoogleAuth.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
 class GoogleAuth extends React.Component {
+    state = { isSignedIn: null };
     componentDidMount() {
         window.gapi.load('client:auth2', () => {
             window.gapi.client.init({
                 clientId: '699970160251-364jnbi331m4saq6gtjsc6dpabvqk8mi.apps.googleusercontent.com',
                 scope: 'email'
-            })
+            }).thhen(() => {
+                this.auth = window.gapi.auth2.getAuthInstance();
+                this.setState({ isSignedIn: this.auth.isSignedIn.get() })
+            });
         });
     }
     
