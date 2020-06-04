86dc5ddf2127e4521a85412dfdc12a4f99d98c8c
csalceda88 <csalceda88@gmail.com>
2020-06-03 23:37:34 -0700

    added redux

--- streams/client/src/index.js
+++ streams/client/src/index.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-
 import App from './components/App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducers from './reducers';
 
-ReactDom.render(<App />, document.querySelector('#root'))
+const store = createStore(reducers);
+
+ReactDom.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+    document.querySelector('#root')
+);
