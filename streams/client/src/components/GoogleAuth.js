b0f4bfa65153e7e727a6203cda54e67697d8e41d
csalceda88 <csalceda88@gmail.com>
2020-06-03 21:15:33 -0700

    signin/out buttoms working

--- streams/client/src/components/GoogleAuth.js
+++ streams/client/src/components/GoogleAuth.js
@@ -2,20 +2,55 @@ import React from 'react';
 
 class GoogleAuth extends React.Component {
     state = { isSignedIn: null };
+
     componentDidMount() {
         window.gapi.load('client:auth2', () => {
-            window.gapi.client.init({
+            window.gapi.client
+            .init({
                 clientId: '699970160251-364jnbi331m4saq6gtjsc6dpabvqk8mi.apps.googleusercontent.com',
                 scope: 'email'
-            }).thhen(() => {
+            }).then(() => {
                 this.auth = window.gapi.auth2.getAuthInstance();
-                this.setState({ isSignedIn: this.auth.isSignedIn.get() })
+                this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+                this.auth.isSignedIn.listen(this.onAuthChange);
             });
         });
     }
+
+    onAuthChange = () => {
+        this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+    };
+
+    onSignIn = () => {
+        this.auth.signIn();
+    };
+
+    onSignOut = () => {
+        this.auth.signOut();
+    };
+
+    renderAuthButton() {
+        if (this.state.isSignedIn === null) {
+            return null;
+        } else if (this.state.isSignedIn) {
+            return (
+                <button onClick={this.onSignOut} className="ui red google button">
+                    <i className="google icon" />
+                    Sign Out
+                </button>
+            );
+        } else {
+            return (
+                <button onClick={this.onSignIn} className="ui red google button">
+                    <i className="google icon" />
+                    Sign In with Google
+                </button>
+            );
+        }
+    }
     
     render() {
-        return <div>GoogleAuth</div>;
+        return <div>{this.renderAuthButton()}</div>;
     }
 }
 
